550c18d3e0f678927d2682dc76edee39daf89efc
Naomi Johnson <naomikaiena@gmail.com>
2019-03-27 20:37:52 -0400

    save/delete

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,12 +1,19 @@
-$(".card").on("click", ".btn", function() {
+$(document).on("click", ".card-body .btn", function(){
   // AJAX POST call to the submit route on the server
   // This will take the data from the form and send it to the server
-  var id = $(this).data("_id");
+  var setStatus = false;
+  if ($(this).text()=== "Save"){
+    setStatus = true;
+  }
+  var id = $(this).data("id");
   $.ajax("/articles/"+ id, {
-    type: "PUT",
+    type: "POST",
     data: {
-      saved: true
+      saved: setStatus
     }
     
   })
 });
+
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
